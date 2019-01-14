2466e11548f61373e5a8bfbc5ffb904bcbc2d09a
melodyphu <melody.phu.8@gmail.com>
2019-01-14 14:05:08 -0500

    fixed int and  date stuff

--- src/models/game.js
+++ src/models/game.js
@@ -1,6 +1,7 @@
 // import node modules
 const mongoose = require('mongoose');
 const ObjectID = mongoose.Schema.Types.ObjectId;
+const Int32 = require('mongoose-int32');
 
 // define a schema
 const GameSchema = new mongoose.Schema ({
@@ -10,14 +11,14 @@ const GameSchema = new mongoose.Schema ({
   jcard_id		: ObjectId, // current judge card
   jdeck			: ObjectId,
   pdeck			: ObjectId,
-  j_wildcards	: Number,
-  p_wildcards	: Number,
+  j_wildcards	: Int32,
+  p_wildcards	: Int32,
   p_discard		: ObjectId,
-  p_discard_wild: Number, // # of discarded wild cards
-  cards_to_win	: Number,
-  timeout 		: Number,
-  time_started	: Number,
-  game_state	: Number, // waiting = 0, pre-play = 1, play = 2, look = 3, judge = 4, game over = 5
+  p_discard_wild: Int32, // # of discarded wild cards
+  cards_to_win	: Int32,
+  timeout 		: Int32,
+  time_started	: Date,
+  game_state	: Int32, // waiting = 0, pre-play = 1, play = 2, look = 3, judge = 4, game over = 5
 
 });
 
