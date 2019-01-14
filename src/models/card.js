f8512080df7918e3de263db799cd26509f3ebc92
Christopher Chang <christopherchang9@gmail.com>
2019-01-14 14:04:32 -0500

    card.js uses Int32

--- src/models/card.js
+++ src/models/card.js
@@ -1,6 +1,7 @@
 // import node modules
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
+const Int32 = require('mongoose-int32');
 
 // define a schema
 const CardSchema = new mongoose.Schema ({
@@ -8,7 +9,7 @@ const CardSchema = new mongoose.Schema ({
   text_creator_id	: ObjectId,
   image_id			: ObjectId,
   image_creator_id	: ObjectId,
-  ref_count			: Number,
+  ref_count			: Int32,
 });
 
 // compile model from schema
