d28f5119e9c54c3a126253d1a848f3eebf1b5b7b
Christopher Chang <christopherchang9@gmail.com>
2019-01-14 14:42:02 -0500

    Image.js component

---
+++ client/src/components/Image.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Image extends Component {
+	constructor(props) {
+		super(props)
+
+		fetch('/api/image', {'_id': this.props.imageId})
+		.then(image => {
+			this.state.image = image;
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<img src={'data:image/jpeg;base64' + this.state.image.toString('base64')}/>
+			</div>
+		);
+	}
+}
+
+export default Image;
