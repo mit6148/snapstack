3260faf667da0a309de7ed620605b86c83fa7038
Christopher Chang <christopherchang9@gmail.com>
2019-01-15 01:23:20 -0500

    skeleton for Card.js

---
+++ client/src/components/Card.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+	constructor(props) { // type {J, P}, faceup (Boolean), cardId; enlarged, [flipStatus]
+		super(props)
+		if (this.props.faceup === undefined) this.props.faceup = true;
+	}
+
+	render() {
+		if (this.props.faceup)
+			return (
+				{null}
+			);
+		else
+			return (
+				{this.props.type == 'J' ? null : null}
+			);
+	}
+}
+
+export default Card;
