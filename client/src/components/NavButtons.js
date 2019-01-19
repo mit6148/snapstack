7c525b2f7f86cec886ae25c9eb3aa442987dac5a
Christopher Chang <christopherchang9@gmail.com>
2019-01-19 02:33:19 -0500

    Simplified NavButtons for pages

--- client/src/components/NavButtons.js
+++ client/src/components/NavButtons.js
@@ -18,31 +18,26 @@ export default class NavButtons extends React.Component {
         switch(this.props.page) {
             case 'Login':
                 return (
-                    <HomeNavBtn appState={this.props.appState} selected={true} />
                     <AboutNavBtn appState={this.props.appState} />
                 );
             case 'Home':
                 return (
-                    <HomeNavBtn appState={this.props.appState} selected={true} />
                     <AboutNavBtn appState={this.props.appState} />
                     <ProfileNavBtn appState={this.props.appState} />
                 );
             case 'GameContainer':
                 return (
-                    <QuitNavBtn appState={this.props.appState} />
+                    null
                 );
             case 'Profile':
                 return (
                     <HomeNavBtn appState={this.props.appState} />
-                    <AboutNavBtn appState={this.props.appState} />
                     <ProfileNavBtn appState={this.props.appState} selected={this.props.params.id === this.props.appState.userId} />
                     <LogoutNavBtn appState={this.props.appState} logout={this.props.logout} />
                 );
             case 'About':
                 return (
                     <HomeNavBtn appState={this.props.appState} />
-                    <AboutNavBtn appState={this.props.appState} selected={true} />
-                    {this.props.appState.userId === null ? null : <ProfileNavBtn appState={this.props.appState} />}
                 );
         }
     }
