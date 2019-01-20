40835bfe9e825cb2dcfec1c612dd6b290a5a3415
Nikhil Singhal <niks@mit.edu>
2019-01-20 15:30:51 -0500

    further shrink whoami

--- server/api.js
+++ server/api.js
@@ -13,21 +13,7 @@ router.get('/test', function (req, res) {
 
 router.get('/whoami', function(req, res) {
     if(req.isAuthenticated()) {
-        UserDetail.findOne({_id: req.user.detail_id}).exec()
-            .then(detail => res.send(
-                {name: detail.name,
-                 //saved_pairs: detail.saved_pairs,
-                 //avatar: detail.avatar,
-                 //description: detail.description,
-                 //email: detail.email,
-                 //media: detail.media,
-                 _id: req.user._id,
-                 currentGameCode: getCurrentGameCode(user)}
-                ))
-            .catch(err => {
-                console.log("failed in whoami");
-                res.send({});
-            });
+        res.send({_id: req.user._id, currentGameCode: getCurrentGameCode(user)});
     } else {
         res.send({});
     }
