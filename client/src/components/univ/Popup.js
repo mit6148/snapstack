eede16ed13c14e229c9604e3b69378b576dc21c0
melodyphu <melody.phu.8@gmail.com>
2019-01-20 16:41:19 -0500

    favicon, working on popup

--- client/src/components/univ/Popup.js
+++ client/src/components/univ/Popup.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "../../css/univ.css";
+
 export default class Popup extends React.Component {
     render() {
         return (
-            <div className="popup">
+            <div className="popup_container">
                 <div>
                     {this.props.children}
                 </div>
