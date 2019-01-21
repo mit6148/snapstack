0eef4a16173d239e5d40b7a7e97e2bb8c6f9617a
Nikhil Singhal <niks@mit.edu>
2019-01-21 14:07:54 -0500

    rename pcard --> pcardref

--- server/models/pcardref.js
+++ server/models/pcardref.js
@@ -4,12 +4,13 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const Int32 = require('mongoose-int32');
 
 // define a schema
-const PCardSchema = new mongoose.Schema ({
+const PCardRefSchema = new mongoose.Schema ({
   text				: String,
   image_ref			: String,
   creator_id        : ObjectId,
   ref_count			: Int32,
+  in_play           : Boolean,
 });
 
 // compile model from schema
-module.exports = mongoose.model('PCard', PCardSchema);
+module.exports = mongoose.model('PCardRef', PCardSchema);
