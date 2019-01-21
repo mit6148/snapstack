371c8c47da415110a25e42ceda0df2cd39e24127
Christopher Chang <christopherchang9@gmail.com>
2019-01-21 04:11:24 -0500

    Added socket listeners to GameContainer. Now all that is left on the front-end is to implement render().

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ const gameStates = {
     JUDGE: 3,
     ROUND_OVER: 4,
     GAME_OVER: 5,
-}
+};
+
+const MIN_PLAYERS = 3;
 
 const MAX_PLAYERS = 7;
 
@@ -13,4 +15,6 @@ const TIME_LIMIT_MILLIS = 120000;
 
 const NUM_JCARDS = 3;
 
-module.exports = {gameStates, MAX_PLAYERS, TIME_LIMIT_MILLIS, NUM_JCARDS};
+const CARDS_TO_WIN = 3;
+
+module.exports = {gameStates, MAX_PLAYERS, TIME_LIMIT_MILLIS, NUM_JCARDS, CARDS_TO_WIN};
