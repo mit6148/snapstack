81653a80e9a43c548d8cde620668dbe43c1fa537
melodyphu <melody.phu.8@gmail.com>
2019-01-22 02:22:01 -0500

    finished the lobby front-end!

--- client/src/components/game/PlayerListCell.js
+++ client/src/components/game/PlayerListCell.js
@@ -4,9 +4,8 @@ import PlayerInfo from "../univ/PlayerInfo";
 export default class PlayerListCell extends React.Component {
     render() {
         return (
-            <div>
                 <PlayerInfo name={this.props.player.name} avatar={this.props.player.avatar} media={this.props.player.media} />
-            </div>
+
         );
     }
 }
--- client/src/components/univ/PlayerMedia.js
+++ client/src/components/univ/PlayerMedia.js
@@ -3,9 +3,14 @@ import React from "react";
 export default class PlayerMedia extends React.Component {
     render() {
         return (
-            <div>
-                <a href={this.props.media.fb}></a>
-                <a href={this.props.media.insta}></a>
+            <div className="player_media">
+            	<div className="fb_btn">
+					<div className="fab fa-facebook-square media_btn fb_logo" href={this.props.media.fb}></div>
+                </div>
+
+                <div className="insta_btn">
+                	<div className="fab fa-instagram media_btn insta_logo" href={this.props.media.insta}></div>
+                </div>
             </div>
         );
     }
