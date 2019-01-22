6aabcd623634d99aed0304b3a09cbd86fbc3ce79
melodyphu <melody.phu.8@gmail.com>
2019-01-21 22:23:29 -0500

    worked on profile and made sections

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -6,19 +6,46 @@ import "../../css/profile.css";
 
 export default class Profile extends React.Component {
     render() {
-        return (
+        return ( //Todo, work on modals for Edit and Cards!
             <div className="profile_text profile_page">
             	<NavButtons appState={this.props.appState} page='Profile' profileId={this.props.id} logout={this.props.logout} />
                 <h1> Profile </h1>
 
-                <div className="picture_container">
-                	<img className="profile_pic" src="/pancakes.jpg"/>
-                </div>
-                <h2> Social Media </h2>
+                <h2> About Me </h2>
+                <div className="about_me">
+	                <div className="picture_container">
+	                	<img className="profile_pic" src="/pancakes.jpg"/>
+	                </div>
+
+	                <div className="user_name">
+	                	First Last
+	                </div>
+
+	                <div className="user_description">
+	                	User Description
+	                </div>
+
+	                <div className="edit_btn"> 
+	                	Edit
+	                </div>
+	            </div>
+
+	            <h2> Social Media </h2>
+	            <div className="social_media">
+	            	<div className="social_media_link">
+	            		Facebook
+	            	</div>
+	            	<div className="edit_btn">
+	            		Edit
+	            	</div>
+	            </div>
+                
               	<h2> My Snapstack </h2>
               	<div className="saved_cards">
-              		
+              		<Card faceup={true} zoome={false}>
+              		</Card>
               	</div>
+
             </div>
         );
     }
