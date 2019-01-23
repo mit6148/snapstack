72f77e2df8a3235d11867128834c9b28bbbdcc53
Nikhil Singhal <niks@mit.edu>
2019-01-22 20:56:20 -0500

    reset callback

--- server/passport.js
+++ server/passport.js
@@ -24,7 +24,7 @@ function getAvatarImagePromise(profile) { // always resolves
 passport.use(new FacebookStrategy({
   clientID: 543314346187914,
   clientSecret: process.env.FB_SECRET,
-  callbackURL: '/auth/facebook/callback',
+  callbackURL: 'https://snap-stack.herokuapp.com/auth/facebook/callback',
   enableProof: true,
   profileFields: ['id', 'first_name', 'last_name', 'picture.type(large)']
 }, function(accessToken, refreshToken, profile, done) {
