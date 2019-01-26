4b7a053c35deafca1baa4dd42f7b437c45b4e004
Nikhil Singhal <niks@mit.edu>
2019-01-26 04:09:08 -0500

    chat title bar

--- client/src/components/game/Chat.js
+++ client/src/components/game/Chat.js
@@ -12,39 +12,33 @@ export default class Chat extends React.Component {
 
         this.state = {
             minimized: false,
-            unread: 0,
-            messages: [["test message asdf asdf asdf asdf", LAZY_B_ID], ["test message asdf asdf asdf asdf", LAZY_B_ID], ["my message", this.props.userId], ["my different message", this.props.userId]] // pairs of form [message, sender name]
+            messages: [["test message asdf asdf asdf asdf", LAZY_B_ID], ["test message asdf asdf asdf asdf", LAZY_B_ID], ["my message", this.props.userId], ["my different message", this.props.userId], ["another message", this.props.userId], ["my different message", this.props.userId], ["my different message", this.props.userId]] // pairs of form [message, sender name]
         }
     }
 
     render() {
         return (
-            <div className="chatLayer">
-                <div className="chatColumn">
-                    <div className="chatBox">
-                        <div className="chatBox-titleBar">
-                            {
-                                "Game Chat" + (this.state.unread > 0 ? (" (" + this.state.unread + ")") : "")
-                            }
-                        </div>
-                        <div className="chatBox-history">
-                            {
-                                this.state.messages.map((pair, index) => (
-                                        <ChatMessage
-                                            message={pair[0]}
-                                            sender={pair[1]}
-                                            previousSender={(this.state.messages[index - 1] || [])[1]}
-                                            nextSender={(this.state.messages[index + 1] || [])[1]}
-                                            playerMap={this.props.playerMap}
-                                            userId={this.props.userId}
-                                            key={index} />
-                                    ))
-                            }
-                        </div>
-                        <div className="chatBox-entry">
-                            {/*TODO*/}
-                        </div>
-                    </div>
+            <div className="chatBox">
+                <div className="chatBox-titleBar">
+                    <div className="chatBox-titleBar-text"> Game Chat </div>
+                    <div className="chatBox-titleBar-minimizeButton"></div>
+                </div>
+                <div className="chatBox-history">
+                    {
+                        this.state.messages.map((pair, index) => (
+                                <ChatMessage
+                                    message={pair[0]}
+                                    sender={pair[1]}
+                                    previousSender={(this.state.messages[index - 1] || [])[1]}
+                                    nextSender={(this.state.messages[index + 1] || [])[1]}
+                                    playerMap={this.props.playerMap}
+                                    userId={this.props.userId}
+                                    key={index} />
+                            ))
+                    }
+                </div>
+                <div className="chatBox-entry">
+                    {/*TODO*/}
                 </div>
             </div>
         );
