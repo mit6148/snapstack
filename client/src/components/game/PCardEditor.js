bf6b7c57340a8f282605036db6def3bac6e2c32f
Nikhil Singhal <niks@mit.edu>
2019-01-27 18:59:52 -0500

    add zoom slider to submit editor

--- client/src/components/game/PCardEditor.js
+++ client/src/components/game/PCardEditor.js
@@ -9,19 +9,27 @@ export default class PCardEditor extends React.Component {
         super(props);
 
         this.imageEditor = React.createRef();
+        this.zoomSlider = React.createRef();
     }
 
 
     render() {
         return (
-            <div>
+            <div style={{"backgroundColor": "blue" }}>
                 <div style={{width: "86px", height: "98px"}}>
                     <ImageEditor image={this.props.image} ref={this.imageEditor}/>
                 </div>
                 <input id="caption-input" type="text" maxLength={MAX_CAPTION_LENGTH} autoComplete="off"/>
                 <div onClick={this.submit}>Submit</div>
+                <input type="range" min="0" max="1" defaultValue="1" step="any"
+                        onChange={this.zoom} onInput={this.zoom} ref={this.zoomSlider}/>
             </div>
         );
+    }
+
+    zoom = e => {
+        this.imageEditor.current.zoom(parseFloat(this.zoomSlider.current.value));
+        return e;
     }
 
 
