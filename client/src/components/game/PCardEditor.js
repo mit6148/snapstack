5be3b6ba53f342cdf93ffffe8bee3bf6969554e6
Nikhil Singhal <niks@mit.edu>
2019-01-27 16:38:13 -0500

    fix child call on editor

--- client/src/components/game/PCardEditor.js
+++ client/src/components/game/PCardEditor.js
@@ -3,16 +3,18 @@ import React from "react";
 import ImageEditor from "./ImageEditor";
 
 export default class PCardEditor extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageEditor = React.createRef();
+    }
+
+
     render() {
-        this.imageEditor = (
-            <ImageEditor image={this.props.image} />
-            );
-
-
         return (
             <div>
-                <div style={{width: "200px", height: "200px"}}>
-                    {this.imageEditor}
+                <div style={{width: "86px", height: "98px"}}>
+                    <ImageEditor image={this.props.image} ref={this.imageEditor}/>
                 </div>
                 <input id="caption-input" type="text" />
                 <div onClick={this.submit}>Submit</div>
@@ -23,7 +25,7 @@ export default class PCardEditor extends React.Component {
 
     submit = () => {
         try {
-            this.props.submit(this.imageEditor.getImage(), document.getElementById('caption-input').value);
+            this.props.submit(this.imageEditor.current.getImage(), document.getElementById('caption-input').value);
         } catch(err) {
             console.log("submit failed: " + err);
         }
