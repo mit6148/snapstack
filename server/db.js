636ea309fe97eb935aeb8f2ba5cd44bcaebcfbf4
Nikhil Singhal <nmsinghal7@gmail.com>
2019-01-29 23:43:25 -0500

    switch to gcp reqs, remove deletion at startup

--- server/db.js
+++ server/db.js
@@ -4,10 +4,9 @@ const path = require('path');
 
 // set up mongoDB connection
 // Example URI ---> mongodb+srv://weblab:6jYctMizX5Y5ie6W@catbook-fsjig.mongodb.net/catbookdb?retryWrites=true
-const mongoURL = ("mongodb+srv://snapstack-server:" + process.env.DB_SECRET
-                  + "@cluster0-guxp5.gcp.mongodb.net/snapstack?retryWrites=true");
+const mongoURL = ("mongodb://snapstack-server:" + process.env.DB_SECRET + "@cluster0-shard-00-00-guxp5.gcp.mongodb.net:27017,cluster0-shard-00-01-guxp5.gcp.mongodb.net:27017,cluster0-shard-00-02-guxp5.gcp.mongodb.net:27017/snapstack?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
 const options = {
-  useNewUrlParser: true,
+  useNewUrlParser: false,
   useCreateIndex: true
 };
 mongoose.connect(mongoURL, options);
